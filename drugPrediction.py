db32f6dfcc3d0aa404b8e46a08b02af6ded2088c
Jayam-Malviya <jayam.malviya@sjsu.edu>
2017-04-12 00:35:41 -0700

    coo matirx

--- drugPrediction.py
+++ drugPrediction.py
@@ -24,14 +24,40 @@ def readTestData():
       document = lines.split(' ')
       test_data.append(document)
 
+from scipy.sparse import  coo_matrix
+import numpy as np
+
+def create_COO_Training():
+  file = './data/train.dat'
+  row_id = 0
+  rows = []
+  cols= []
+  data = []
+  with open(file) as f1:
+    for lines in f1:
+      lines = lines.replace('\n',' ').replace('\t',' ')
+      lines = ' '.join(lines.split())
+      document = lines.split(' ')
+      train_labels.append(document[0])
+      for i in range(1,len(document)):
+        rows.append(row_id)
+        cols.append(document[i])
+        data.append(1)
+      row_id += 1
+  rows = np.array(rows)
+  cols = np.array(cols)
+  data = np.array(data)
+  print(len(rows))
+  print(len(cols))
+  print(len(data))
+  train_coo_matrix = coo_matrix((data,(rows,cols)), shape=(800,100001))
+  return train_coo_matrix
 
 def main():
-  readTrainingData()
-  print(len(features))
-
-  readTestData()
-  print(test_data[1])
-
+  # readTrainingData()
+  # readTestData()
+  train_coo_matrix = create_COO_Training()
+  print(train_coo_matrix.toarray())
 
 if __name__ == '__main__':
   main()
